ac39cbdfb79eee2a8f106b83c3d20807a293e4aa
VictorSanchezCribier <victor.sanchez0508@gmail.com>
2023-02-27 17:33:29 +0100

    exo5b

--- amse_tp2/lib/exo5b.dart
+++ amse_tp2/lib/exo5b.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'exo4.dart' as exo4;
 class DisplayGridWidget extends StatelessWidget {
-  exo4.Tile Tile = new exo4.Tile(
+  exo4.Tile tile = new exo4.Tile(
       imageURL: 'https://picsum.photos/512', alignment: Alignment(0, 0));
   
   @override
@@ -12,49 +12,21 @@ class DisplayGridWidget extends StatelessWidget {
       mainAxisSpacing: 10,
       children: <Widget>[
         Container(
-          child: exo4.
+          child: exo4.DisplayTileWidget().createTileWidgetFrom(tile),
         ),
         Container(),
         Container(),
         Container(),
         Container(),
         Container(
-          color: Colors.orange,
-          child: Center(
-            child: Text(
-              "Tile 6",
-              style: TextStyle(
-                  color: Colors.black, decoration: TextDecoration.none),
+          
+        ),
+        Container(
             ),
-          ),
-        ),
         Container(
-            color: Colors.yellow,
-            child: Center(
-              child: Text(
-                "Tile 7",
-                style: TextStyle(
-                    color: Colors.black, decoration: TextDecoration.none),
-              ),
-            )),
+           ),
         Container(
-            color: Colors.green,
-            child: Center(
-              child: Text(
-                "Tile 8",
-                style: TextStyle(
-                    color: Colors.black, decoration: TextDecoration.none),
-              ),
-            )),
-        Container(
-            color: Colors.lightGreenAccent,
-            child: Center(
-              child: Text(
-                "Tile 9",
-                style: TextStyle(
-                    color: Colors.black, decoration: TextDecoration.none),
-              ),
-            ))
+            )
       ],
     );
   }
