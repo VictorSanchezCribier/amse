082e2af1204801add3c0f83db90db5c8cac32ba2
VictorSanchezCribier <victor.sanchez0508@gmail.com>
2023-03-12 18:27:44 +0100

    Même chose mais avec une grille 3x3

--- amse_tp2/lib/exo6.dart
+++ amse_tp2/lib/exo6.dart
@@ -8,13 +8,12 @@ import 'dart:math' as math;
 math.Random random = new math.Random();
 
 class Tile {
-  Color color = Colors.transparent;
+  Color color;
 
   Tile(this.color);
-  Tile.randomColor() {
-    this.color = Color.fromARGB(
-        255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
-  }
+  Tile.randomColor()
+      : color = Color.fromARGB(
+            255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
 }
 
 // ==============
@@ -23,9 +22,9 @@ class Tile {
 
 class TileWidget extends StatefulWidget {
   final Tile tile;
-  final Function onTap;
+  final VoidCallback onPressed;
 
-  TileWidget(this.tile, {required this.onTap});
+  TileWidget({required this.tile, required this.onPressed});
 
   @override
   _TileWidgetState createState() => _TileWidgetState();
@@ -35,15 +34,12 @@ class _TileWidgetState extends State<TileWidget> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        widget.onTap();
-      },
+      onTap: widget.onPressed,
       child: Container(
-        color: widget.tile.color,
-        child: Padding(
-          padding: EdgeInsets.all(70.0),
-        ),
-      ),
+          color: widget.tile.color,
+          child: Padding(
+            padding: EdgeInsets.all(70.0),
+          )),
     );
   }
 }
@@ -56,7 +52,7 @@ class PositionedTiles extends StatefulWidget {
 }
 
 class PositionedTilesState extends State<PositionedTiles> {
-  List<Tile> tiles = List<Tile>.generate(2, (index) => Tile.randomColor());
+  List<Tile> tiles = List<Tile>.generate(9, (index) => Tile.randomColor());
 
   @override
   Widget build(BuildContext context) {
@@ -65,22 +61,23 @@ class PositionedTilesState extends State<PositionedTiles> {
         title: Text('Moving Tiles'),
         centerTitle: true,
       ),
-      body: Row(
-        children: tiles.asMap().entries.map((entry) {
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: tiles.map((tile) {
           return TileWidget(
-            entry.value,
-            onTap: () {
-              swapTiles(entry.key);
+            tile: tile,
+            onPressed: () {
+              setState(() {
+                int currentIndex = tiles.indexOf(tile);
+                int newIndex = (currentIndex + 1) % tiles.length;
+                Tile temp = tiles[currentIndex];
+                tiles[currentIndex] = tiles[newIndex];
+                tiles[newIndex] = temp;
+              });
             },
           );
         }).toList(),
       ),
     );
-  }
-
-swapTiles(int key) {
-    setState(() {
-      tiles.insert(1, tiles.removeAt(0));
-    });
   }
 }
